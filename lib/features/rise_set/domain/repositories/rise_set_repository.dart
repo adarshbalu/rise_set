eb76e651cbe3b8f3fe4c581cafc8fe2269b2e0d9
Adarsh Balachandran <adarshlp98@gmail.com>
2020-10-29 21:10:50 +0530

    Added rise_set_repository.dart in domain repository as abstract

---
+++ lib/features/rise_set/domain/repositories/rise_set_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:rise_set/core/error/failures.dart';
+import 'package:rise_set/features/rise_set/domain/entities/rise_set.dart';
+
+abstract class RiseSetRepository {
+  Future<Either<Failure, RiseSet>> getRiseAndSetTime();
+}
