32c06cba2a8c46b38fb286a939e4b86566ea216f
Adarsh Balachandran <adarshlp98@gmail.com>
2020-11-11 15:02:48 +0530

    Added color constant strings

--- lib/core/util/constants.dart
+++ lib/core/util/constants.dart
@@ -5,3 +5,9 @@ const String INVALID_INPUT_FAILURE_MESSAGE =
     'Invalid Input - The number must be a double or zero.';
 
 enum Status { LOADED, ERROR, LOADING }
+
+const DEEP_BLUE = '00C0F1';
+const DARK_BLUE = '2AD8F6';
+const LIGHT_BLUE = 'A8EBFA';
+const DARK_YELLOW = 'FFD400';
+const LIGHT_YELLOW = 'FFE470';
