dd6d8702c351746f305597ce5999d29ca6f5fec7
Adarsh Balachandran <adarshlp98@gmail.com>
2020-11-11 13:59:40 +0530

    Added dependency container

---
+++ lib/dependency_container.dart
@@ -0,0 +1,56 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:rise_set/core/network/network_info.dart';
+import 'package:rise_set/core/util/input_converter.dart';
+import 'package:rise_set/features/rise_set/data/datasources/rise_set_local_datasource.dart';
+import 'package:rise_set/features/rise_set/data/datasources/rise_set_remote_datasource.dart';
+import 'package:rise_set/features/rise_set/data/repositories/rise_set_repository_impl.dart';
+import 'package:rise_set/features/rise_set/domain/repositories/rise_set_repository.dart';
+import 'package:rise_set/features/rise_set/domain/usecases/get_rise_and_set_time.dart';
+import 'package:rise_set/features/rise_set/presentation/providers/rise_set_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final locator = GetIt.instance;
+Future<void> init() async {
+  //features
+
+  //providers
+
+  locator.registerFactory(
+    () => RiseSetProvider(
+      locator(),
+      locator(),
+    ),
+  );
+
+  //usecases
+  locator.registerLazySingleton(() => GetRiseAndSetTime(locator()));
+
+  //repository
+
+  locator.registerLazySingleton<RiseSetRepository>(() => RiseSetRepositoryImpl(
+      remoteDataSource: locator(),
+      localDataSource: locator(),
+      networkInfo: locator()));
+
+  //data sources
+
+  locator.registerLazySingleton<RiseSetRemoteDataSource>(
+      () => RiseSetRemoteDataSourceImpl(
+            client: locator(),
+          ));
+  locator.registerLazySingleton<RiseSetLocalDataSource>(
+      () => RiseSetLocalDataSourceImpl(sharedPreferences: locator()));
+
+  //core
+
+  locator.registerLazySingleton(() => InputConverter());
+  locator.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(locator()));
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  locator.registerLazySingleton(() => sharedPreferences);
+  locator.registerLazySingleton(() => http.Client());
+  locator.registerLazySingleton(() => DataConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rise_set/features/rise_set/presentation/pages/home/home_page.dart';
+import 'package:rise_set/features/rise_set/presentation/providers/rise_set_provider.dart';
+import 'dependency_container.dart' as di;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  di.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RISE SET',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => di.locator<RiseSetProvider>(),
+          lazy: true,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'RISE SET',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
