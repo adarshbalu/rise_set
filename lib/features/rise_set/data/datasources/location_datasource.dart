f8492762e5da9848ba55cf78067844565041f0d8
Adarsh Balachandran <adarshlp98@gmail.com>
2020-11-14 23:01:30 +0530

    Added location data source and test

--- lib/features/rise_set/data/datasources/location_datasource.dart
+++ lib/features/rise_set/data/datasources/location_datasource.dart
@@ -1,6 +1,18 @@
 import 'package:location/location.dart';
+import 'package:meta/meta.dart';
+import 'package:rise_set/features/rise_set/data/models/location_model.dart';
 
 abstract class LocationDataSource {
-  Future<LocationData> getCurrentUserLocation();
-  Future<bool> getLocationAccessPermissions();
+  Future<LocationModel> getCurrentUserLocation();
+}
+
+class LocationDataSourceImpl extends LocationDataSource {
+  final Location location;
+
+  LocationDataSourceImpl({@required this.location});
+  @override
+  Future<LocationModel> getCurrentUserLocation() async {
+    final locationData = await location.getLocation();
+    return LocationModel.fromLocationData(locationData);
+  }
 }
---
+++ test/features/rise_set/data/datasources/location_datasource_test.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:location/location.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:rise_set/features/rise_set/data/datasources/location_datasource.dart';
+import 'package:rise_set/features/rise_set/data/models/location_model.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+//class MockLocationDataSource extends Mock implements LocationDataSource {}
+
+class MockLocationInfo extends Mock implements Location {}
+
+void main() {
+  LocationDataSourceImpl dataSource;
+  MockLocationInfo mockLocationInfo;
+
+  setUp(() {
+    mockLocationInfo = MockLocationInfo();
+    dataSource = LocationDataSourceImpl(location: mockLocationInfo);
+  });
+
+  group('get location data', () {
+    final tLocation = LocationModel(latitude: 10.0, longitude: 20.0);
+    test(
+        'should return a locationModel from location package if there is permission',
+        () async {
+      final fixtureResult = fixture('location_data.json');
+      final Map<String, dynamic> tdataMap = jsonDecode(fixtureResult);
+      final Map<String, double> fixedMap = {
+        'latitude': tdataMap['latitude'],
+        'longitude': tdataMap['longitude']
+      };
+      final LocationData locationData = LocationData.fromMap(fixedMap);
+      when(mockLocationInfo.getLocation())
+          .thenAnswer((_) async => locationData);
+      final result = await dataSource.getCurrentUserLocation();
+      expect(result, equals(tLocation));
+    });
+  });
+}
